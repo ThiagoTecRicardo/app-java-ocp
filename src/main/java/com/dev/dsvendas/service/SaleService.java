6649fd1548496e72cc60277a2c82bd4325e7794b
Thiago Ricardo <thiago.tec.ricardo@gmail.com>
2021-11-10 04:00:06 -0300

    create repo

---
+++ src/main/java/com/dev/dsvendas/service/SaleService.java
@@ -0,0 +1,56 @@
+package com.dev.dsvendas.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dev.dsvendas.dto.SaleDTO;
+import com.dev.dsvendas.dto.SaleSuccessDTO;
+import com.dev.dsvendas.dto.SaleSumDTO;
+import com.dev.dsvendas.entities.Sale;
+import com.dev.dsvendas.repositories.SaleRepository;
+import com.dev.dsvendas.repositories.SellerRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository saleRepository;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+
+	@Transactional(readOnly = true)
+	public Page<SaleDTO> findAll(Pageable pageable) {
+		
+		sellerRepository.findAll();
+
+		Page<Sale> ressult =  saleRepository.findAll(pageable);
+		
+		return ressult.map(x -> new SaleDTO(x));
+
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSumDTO> amountGroupedBySeller(){
+		
+		return saleRepository.amountGroupedBySeller();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<SaleSuccessDTO> seccessGroupedBySeller(){
+		
+		return saleRepository.seccessGroupedBySeller();
+		
+	}
+	
+	
+	
+
+}
+ 	
